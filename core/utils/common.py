ab9fc5faddde14278aa651f1457a002129152d78
Eun0 <prt1004dms@naver.com>
2022-01-12 23:21:15 +0900

    Add set seed

--- core/utils/common.py
+++ core/utils/common.py
@@ -1,4 +1,12 @@
-
+import random
+import numpy as np
+import torch 
 
 def count_params(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
